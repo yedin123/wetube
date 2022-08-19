4e23da98e2b82fff8eb686991146a8ec5952256b
yedin123 <73570623+yedin123@users.noreply.github.com>
2022-08-19 17:05:47 +0900

    templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,32 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title:"First video",
+            rating:5,
+            comments:2,
+            createdAt:"50 minutes ago",
+            views:10032,
+            id:1
+        },
+        {
+            title:"Second video",
+            rating:5,
+            comments:2,
+            createdAt:"10 minutes ago",
+            views:10032,
+            id:2
+        },{
+            title:"Third video",
+            rating:5,
+            comments:2,
+            createdAt:"2 minutes ago",
+            views:10032,
+            id:3
+        }
+    ]
+    return res.render("home", {pageTitle: "Home", videos: videos})
+};
 
 export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
 
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { join, login } from "../controllers/userController";
-import { trending, search } from "../controllers/videoController";
+import { join, login, logout } from "../controllers/userController";
+import { trending, search, } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 globalRouter.get("/", trending);
 globalRouter.get("/join"/ join);
-globalRouter.get("/loing"/ login);
+globalRouter.get("/login"/ login);
 globalRouter.get("/search"/ search);
+globalRouter.get("/logout"/ logout);
 
 export default globalRouter;
