374d395c7fb54f719663b3571f13450d2560612b
yedin123 <73570623+yedin123@users.noreply.github.com>
2022-08-26 01:30:24 +0900

    Login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,25 +1,30 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => {
     res.render("join", {pageTitle: "Join"});
 }
 export const postJoin = async (req, res) => {
+    const pageTitle = "Join";
     const {name, email, username, password, location} = req.body;
     const userNameExists = await User.exists({username});
     if(userNameExists) 
-        return res.status(400).render("/join", {pageTitle:"Join", errorMessage:"This username is already taken."}); 
+        return res.status(400).render("/join", {pageTitle: pageTitle, errorMessage:"This username is already taken."}); 
     const emailNameExists = await User.exists({email});
     if(emailNameExists) 
-        return res.status(400).render("/join", {pageTitle:"Join", errorMessage:"This email is already taken."});
-    await User.create({
-        name,
-        email,
-        username,
-        password,
-        location,
-    });
-
-    return res.redirect("/login");
+        return res.status(400).render("/join", {pageTitle: pageTitle, errorMessage:"This email is already taken."});
+    try{
+        await User.create({
+            name,
+            email,
+            username,
+            password,
+            location,
+        });
+        return res.redirect("/login");
+    } catch(error){
+        return res.status(400).render("join", {pageTitle: pageTitle, errorMessage: error.message} );
+    }
 }
 export const edit = (req, res) =>{
     res.send("Edit User");
@@ -27,8 +32,20 @@ export const edit = (req, res) =>{
 export const remove = (req, res) =>{
     res.send("Remove User");
 }
-export const login = (req, res) =>{
-    res.send("Login");
+export const getLogin = (req, res) =>{
+    res.render("login", { pageTitle: "Login" });
+}
+export const postLogin = async (req, res) =>{
+    const { username, password } = req.body;
+    const user = await User.findOne({username});
+    console.log(password);
+    console.log(user.password);
+    if(!user)
+        return res.status(400).render("/login", { pageTitle: "Login", errorMessage: "An account with this username does not exist." });
+    const ok = await bcrypt.compare(password, user.password);
+    if(!ok)
+        return res.status(400).render("/login", { pageTitle: "Login", errorMessage: "Wrong password" });
+    return res.redirect("/");
 }
 export const logout = (req, res) =>{
     res.send("Logout");
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { getJoin, postJoin, login, logout } from "../controllers/userController";
+import { getJoin, postJoin, getLogin, postLogin, logout } from "../controllers/userController";
 import { home, search, } from "../controllers/videoController";
 
 const rootRouter = express.Router();
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getLogin).post(postLogin);
 rootRouter.get("/search", search);
 rootRouter.get("/logout", logout);
 rootRouter.get("/search", search);
